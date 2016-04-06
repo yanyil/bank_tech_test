570bf9fc055ac1760a8449be44c94ba75c7ca471
Yan-Yi Li <liyanyi@gmail.com>
2016-04-06 11:28:45 +0100

    added #deposit for Account

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,12 @@
 class Account
-  def balance
-    0
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(amount)
+    @balance += amount
+    amount
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,15 @@ describe Account do
       expect(account.balance).to eq 0
     end
   end
+
+  describe '#deposit' do
+    it 'can make a deposit' do
+      account.deposit(1000)
+      expect(account.balance).to eq 1000
+    end
+
+    it 'returns the amount that was deposited' do
+      expect(account.deposit(1000)).to eq 1000
+    end
+  end
 end
