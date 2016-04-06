5e21558b2074d169d7f9876afff8ec7775f10ed0
Yan-Yi Li <liyanyi@gmail.com>
2016-04-06 15:35:57 +0100

    refactored to format amount entry

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,11 +8,7 @@ class StatementPrinter
 
   def print_out
     @account.statement.reverse.each do |entry|
-      if entry[:amount] > 0
-        @history << "#{format_date(entry[:date])} || #{format_currency(entry[:amount])} || || #{format_currency(entry[:balance])}"
-      else
-        @history << "#{format_date(entry[:date])} || || #{format_currency(-entry[:amount])} || #{format_currency(entry[:balance])}"
-      end
+      add_history(entry, entry[:amount])
     end
     @history.join("\n")
   end
@@ -25,5 +21,17 @@ class StatementPrinter
 
   def format_currency(amount)
     '%.2f' % amount
+  end
+
+  def format_amount(entry, amount)
+    if entry[:amount] > 0
+      "#{format_currency(entry[:amount])} ||"
+    else
+      "|| #{format_currency(-entry[:amount])}"
+    end
+  end
+
+  def add_history(entry, amount)
+    @history << "#{format_date(entry[:date])} || #{format_amount(entry, amount)} || #{format_currency(entry[:balance])}"
   end
 end
